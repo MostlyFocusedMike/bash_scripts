8d6b13620c64286cab5fac32fbbc0338f79a0f51
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2017-10-15 23:28:13 -0400

    edited cleaner

---
+++ desktop_cleaner.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv ~/Desktop/*.txt ~/Desktop/txt-files &> /dev/null
+mv ~/Desktop/*.{stl,gcode} ~/Desktop/3d-printing &> /dev/null
+mv ~/Desktop/Screen* ~/Desktop/screenshots &> /dev/null
+mv ~/Desktop/*.{jpg,jpeg,png} ~/Desktop/desktop-pics &> /dev/null
+mv ~/Desktop/*.dmg ~/Desktop/coding &> /dev/null
+echo all clean! 
